362a4b0f0ed1bab810da62782f31e17f6da817e8
=nontiwabuathong <nontiwa.bt@gmail.com>
2019-03-08 14:36:41 +0700

    add many things

--- AppValidator.js
+++ AppValidator.js
@@ -1,1 +1,10 @@
-console.log('AppValidator.js is working');
+// import validator from 'validator';
+var validator = require('validator');
+
+
+
+let text = 'foo@bar.com';
+let result = validator.isEmail(text);
+
+console.log(text, 'is', result);
+// console.log('text is', text);
